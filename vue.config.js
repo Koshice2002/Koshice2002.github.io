eef2eb19683900ff4b106d723ed00b3bf1539b85
Koshice <khoa93.pch@gmail.com>
2023-03-25 23:19:09 +0700

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Koshice2002.github.io/' // Thay tên repository của các bạn vào đây nhé
+    : '/',
   transpileDependencies: [
     
   ]
