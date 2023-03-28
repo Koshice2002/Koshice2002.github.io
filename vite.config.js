a48b4d862b95cd672e377126038ab6d3b33055bf
Koshice <khoa93.pch@gmail.com>
2023-03-28 15:23:27 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/Koshice2002.github.io/',
     plugins: [
         vue(),
         laravel({
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Koshice0310/' // Thay tên repository của các bạn vào đây nhé
+    ? '/Koshice2002.github.io/'
     : '/',
   transpileDependencies: [
-    
   ]
 })
